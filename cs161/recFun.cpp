3019088476b13b09bcdbe88dd59ad815ea377f0e
Lindsay Tucker <felker.lindsay@gmail.com>
2015-07-28 20:45:14 -0700

    tidying

--- cs161/recFun.cpp
+++ cs161/recFun.cpp
@@ -4,7 +4,7 @@ Date created: 31Oct2014
 Date last modified: 31Oct2014
 Program Filename:  recFun.cpp
 Description/overview: 
-This program will accept integer input  (n) that is sent to a (recursive) function to 
+This program will accept integer input (n) that is sent to a (recursive) function to
 determine the nth number in the Fibonacci sequence.
 
 INPUT: 
@@ -36,9 +36,7 @@ int main ()
 		cin>> input;
 		cout<< "This program will display the "<< input<<"th Fibonacci number." <<endl;
 		
-		cout<<fib(input-1)<<endl; //simplified version of next 2 lines
-			//fib(input);
-			//cout<< fib(input-1)<< endl; //Input-1 displays the accurate input's number in the sequence, sequence starts at F sub 0
+		cout<<fib(input-1)<<endl; //Input-1 displays the accurate input's number in the sequence, sequence starts at F sub 0
 		
 		return 0;
 }
@@ -50,7 +48,7 @@ int main ()
 
 int fib(int input) 
 {
-			if (input<=0) //BASE CASE
+			if (input<=0)      //BASE CASE
 				return 0;
 			else if (input==1) //BASE CASE
 				return 1;
